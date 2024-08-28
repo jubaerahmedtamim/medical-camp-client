a594ef6d19714a92fbffd3bca73905914f82e357
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-08-29 05:06:02 +0600

    register camp join done

--- src/Pages/AvailableCamp/CampDetails.jsx
+++ src/Pages/AvailableCamp/CampDetails.jsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import { FaLocationDot } from 'react-icons/fa6';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 import SectionTitle from '../../components/SectionTitle';
+import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
 
 const CampDetails = () => {
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const navigate = useNavigate();
     const { data } = useLoaderData()
-    const { campName, campFees, location, date, time, professionalName, details, image_url } = data;
-    
+    const { campName, campFees, location, date, time, professionalName, details, image_url, _id } = data;
+
+    const handleJoinCamp = (id) => {
+        const registeredCamp = {
+            cmapId: id,
+            email: user?.email,
+            participantName: user?.displayName,
+            campName,
+            campFees,
+            image_url,
+        }
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You want to join this camp?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, I want to Join.!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.post('/registered-camps', registeredCamp)
+                console.log(res.data.insertedId);
+                if (res.data.insertedId) {
+                    Swal.fire({
+                        title: "Done",
+                        text: "You have successfully joined this camp.",
+                        icon: "success"
+                    });
+                    navigate(-1)
+                }
+
+            }
+        });
+    }
+
+
     return (
         <div>
             <SectionTitle heading={"Camp Details"} subHeading={"Here is the details of each camp."}></SectionTitle>
@@ -25,7 +66,7 @@ const CampDetails = () => {
                     <img className='rounded-lg' src={image_url} alt="image of camp" />
                 </div>
                 <p className=' max-w-screen-2xl md:max-w-screen-md mx-auto'>{details}</p>
-                <button className='btn btn-primary'>Join Camp</button>
+                <button onClick={() => handleJoinCamp(_id)} className='btn btn-primary'>Join Camp</button>
             </div>
         </div>
     );
