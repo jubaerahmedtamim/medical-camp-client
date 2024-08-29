683fb4c50ac2e34a84d2a9303efa36da9a5f5884
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-08-30 01:34:58 +0600

    normal user can cancel the registered camp

--- src/Pages/DashboardPages/Participants/RegisterCamp/RegisterCamp.jsx
+++ src/Pages/DashboardPages/Participants/RegisterCamp/RegisterCamp.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 import useAuth from '../../../../hooks/useAuth';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const RegisterCamp = () => {
     const { user } = useAuth();
@@ -10,12 +11,40 @@ const RegisterCamp = () => {
 
     const { data: registeredCamps = [], isLoading, refetch } = useQuery({
         queryKey: ["registeredCamps", user?.email],
+        enabled: !!user?.email,
         queryFn: async () => {
-            const res = await axiosSecure.get(`/registered-camps?email=${user?.email}`);
+            const res = await axiosSecure.get(`/registered-camps/${user?.email}`);
             // console.log(res.data);
             return res.data
         }
     })
+
+    const handleRemoveCamp = (id)=> {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then(async(result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/delete-registered-camp/${id}`)
+                if(res.data.deletedCount > 0){
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "This camp has been deleted.",
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                }
+            }
+          });
+    }
+
     return (
         <div>
             <h1 className='text-2xl'>Total Joined Camps: {registeredCamps.length} </h1>
@@ -50,10 +79,10 @@ const RegisterCamp = () => {
                                     }
                                 </td>
                                 <td>
-                                    <button className='btn btn-xs btn-error'>Cancel</button>
+                                    <button onClick={()=>handleRemoveCamp(camp._id)} disabled={camp.payment_status === 'paid' || camp.confirmation_status ==='Confirmed'} className='btn btn-xs btn-error'>Cancel</button>
                                 </td>
                                 <td>
-                                    <button disabled className='btn btn-sm btn-success'> Give feedback</button>
+                                    <button disabled={camp.confirmation_status === 'Pending' || camp.payment_status === 'unpaid' } className='btn btn-sm btn-success'> Give feedback</button>
                                 </td>
                             </tr>)
                         }
